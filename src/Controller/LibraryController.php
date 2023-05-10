1a3a040e223c6b289e5650786092bba17d8d7d01
elan19 <elan19@student.bth.se>
2023-05-10 15:33:13 +0200

    Gjort formuläret för uppgiften samt att man kan läsa detaljer om en bok

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Book;
+use App\Form\BookType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LibraryController extends AbstractController
 {
@@ -13,6 +17,37 @@ class LibraryController extends AbstractController
     {
         return $this->render('library/index.html.twig', [
             'controller_name' => 'LibraryController',
+        ]);
+    }
+
+    /**
+     * @Route("/books/new", name="book_new")
+     */
+    #[Route('/library/add', name: 'library_add')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($book);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('library_home');
+        }
+
+        return $this->render('library/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    #[Route('/library/{id}', name: 'library_show')]
+    public function show(Book $book): Response
+    {
+        return $this->render('library/show.html.twig', [
+            'book' => $book,
         ]);
     }
 }
