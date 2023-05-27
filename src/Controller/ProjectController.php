b4249f7496caae46e2f78b2e6e37087afa7fcaaa
elan19 <elan19@student.bth.se>
2023-05-27 21:58:38 +0200

    Ändrat lite för att kunna göra så spelarna kan köra om och om igen

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -35,8 +35,9 @@ class ProjectController extends AbstractController
             ->getForm();
 
         $form->handleRequest($request);*/
-
-        $session->set('setupInProgress', false);
+        if ($session->has('gameInProgress') == false) {
+            $session->set('gameInProgress', false);
+        }
 
         /*if ($form->isSubmitted() && $form->isValid()) {
             $numPlayers = $form->getData()['numPlayers'];
@@ -45,7 +46,7 @@ class ProjectController extends AbstractController
             //return $this->redirectToRoute('blackjack_setup', ['numPlayers' => $numPlayers]);
         }*/
 
-        return $this->render('project/home.html.twig');
+        return $this->render('project/home.html.twig', ['game' => $session->get('gameInProgress')]);
     }
 
 
@@ -60,11 +61,39 @@ class ProjectController extends AbstractController
     {
         $numPlayers = $request->request->get('numPlayers');
 
+        if($numPlayers == null) {
+            return $this->redirectToRoute('proj_home');
+        }
+
         // Check if the form is submitted and the number of players is valid
         if ($request->isMethod('POST') && $numPlayers >= 1 && $numPlayers <= 3) {
             // Store the number of players in the session
             $session->set('numPlayers', $numPlayers);
         }
+            // Redirect to the blackjack route
+            //return $this->redirectToRoute('blackjack', ['numPlayers' => (int)$numPlayers]);
+
+        return $this->render('project/setup.html.twig', [
+            'numPlayers' => $numPlayers,
+            'game' => $session->get('gameInProgress'),
+        ]);
+    }
+
+    #[Route("/proj/blackjack/bet", name: "blackjack_bet")]
+    public function blackjackBet(Request $request, SessionInterface $session): Response
+    {
+        $numPlayers = $session->get('numPlayers');
+
+        if($numPlayers == null) {
+            return $this->redirectToRoute('proj_home');
+        }
+
+        // Check if the form is submitted and the number of players is valid
+        /*if ($request->isMethod('POST') && $numPlayers >= 1 && $numPlayers <= 3) {
+            // Store the number of players in the session
+            $session->set('numPlayers', $numPlayers);
+        }*/
+        var_dump($numPlayers);
 
         if ($request->isMethod('POST') && $request->request->has('playerName1')) {
             $playerNames = [];
@@ -73,64 +102,93 @@ class ProjectController extends AbstractController
                 $playerNames[$i] = $playerName;
             }
             $session->set('playerNames', $playerNames);
-            var_dump($playerNames);
-        
-            // Redirect to the blackjack route
-            //return $this->redirectToRoute('blackjack', ['numPlayers' => (int)$numPlayers]);
         }
 
-        return $this->render('project/setup.html.twig', [
+        if (!$session->has('playerHands')) {
+            $playerHands = [];
+            for ($i = 0; $i < $numPlayers; $i++) {
+                $playerHands[$i] = new CardHand();
+            }
+            $session->set('playerHands', $playerHands);
+        }
+        $playerHands = $session->get('playerHands', []);
+        $playerNames = $session->get('playerNames', []);
+        var_dump($playerHands);
+        var_dump($playerNames);
+
+        if (!$session->has('betInProgress')) {
+            $session->set('betInProgress', true);
+        }
+
+        return $this->render('project/bet.html.twig', [
             'numPlayers' => $numPlayers,
-            'game' => $session->get('setupInProgress'),
+            'playerHands' => $playerHands,
+            'playerNames' => $playerNames,
+            'game' => $session->get('gameInProgress'),
+            'bet' => $session->get('betInProgress'),
         ]);
     }
 
     #[Route("/proj/blackjack", name: "blackjack_solo")]
     public function blackjack_solo(): Response
     {
-        return $this->redirectToRoute('blackjack', ['numPlayers' => 1]);
+        return $this->redirectToRoute('blackjack/game');
     }
 
-    #[Route("/proj/blackjack/{numPlayers}", name: "blackjack")]
-    public function blackjack(Request $request, int $numPlayers, SessionInterface $session): Response
+    #[Route("/proj/blackjack/game", name: "blackjack_game")]
+    public function blackjack(Request $request, SessionInterface $session): Response
     {
-        $deck = $session->get('deck');
-        $playerHands = $session->get('playerHands', []);
-        $dealerHand = $session->get('dealerHand');
-        if ($session->get('setupInProgress')) {
-            $session->set('setupInProgress', true);
+        $numPlayers = $session->get('numPlayers');
+
+        if(empty($numPlayers)) {
+            return $this->redirectToRoute('proj_home');
         }
 
-        if ($request->isMethod('POST') && $request->request->has('playerName1')) {
-            $playerNames = [];
+        $deck = $session->get('deck');
+        $playerHands = $session->get('playerHands', []);
+
+        /*if(!$playerHands) {
+            return $this->redirectToRoute('blackjack_setup');
+        }*/
+
+        if ($request->isMethod('POST') && $request->request->has('betAmount1')) {
             $playerBets = [];
             for ($i = 1; $i <= $numPlayers; $i++) {
-                $playerName = $request->request->get("playerName$i");
                 $playerBet = $request->request->get("betAmount$i");
-                $playerNames[$i] = $playerName;
                 $playerBets[$i] = $playerBet;
+                if (($playerHands[$i-1]->getTotalMoney() - $playerBet) < 0) {
+                    return $this->redirectToRoute('blackjack_bet');
+                }
             }
-            $session->set('playerNames', $playerNames);
             $session->set('playerBets', $playerBets);
-            var_dump($playerNames);
         }
         $playerNames = $session->get('playerNames', []);
         $playerBets = $session->get('playerBets', []);
 
         var_dump($playerNames);
         var_dump($playerBets);
+        var_dump($playerHands);
+
+        $dealerHand = $session->get('dealerHand');
+
+        if ($session->get('gameInProgress') == false) {
+            $session->set('gameInProgress', true);
+            $session->set('betInProgress', false);
+        }
 
         // Initialize the game if the session data is not available
         if (!$deck || !$playerHands || !$dealerHand) {
             $deck = new DeckOfCards();
             $deck->shuffle();
 
-            $playerHands = [];
             for ($i = 0; $i < $numPlayers; $i++) {
-                $playerHand = new CardHand();
-                $playerHand->addCard($deck->drawCard());
-                $playerHand->addCard($deck->drawCard());
-                $playerHands[] = $playerHand;
+                $playerHands[$i]->addCard($deck->drawCard());
+                $playerHands[$i]->setBet($playerBets[$i+1]);
+                $playerHands[$i]->updateTotalMoney(-$playerBets[$i+1]);
+            }
+
+            for ($i = 0; $i < $numPlayers; $i++) {
+                $playerHands[$i]->addCard($deck->drawCard());
             }
 
             $dealerHand = new CardHand();
@@ -155,6 +213,15 @@ class ProjectController extends AbstractController
 
                     if ($action === 'hit') {
                         $playerHand->addCard($deck->drawCard());
+                        if ($playerHand->isBust()) {
+                            // Move to the next player
+                            $currentPlayer = $session->get('currentPlayer');
+                            $currentPlayer++;
+                            if ($currentPlayer > $numPlayers) {
+                                $currentPlayer = 1; // Start over from the first player
+                            }
+                            $session->set('currentPlayer', $currentPlayer);
+                        }
                     } elseif ($action === 'stand') {
                         $playerHand->stand();
 
@@ -194,7 +261,6 @@ class ProjectController extends AbstractController
             'dealerHand' => $dealerHand,
             'playerHands' => $playerHands,
             'playerNames' => $playerNames,
-            'playerBets' => $playerBets,
             'session' => $session,
         ]);
     }
